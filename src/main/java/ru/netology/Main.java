2e6d3f3bea05780cfc1d7c7f660858213d663517
Levin-Alexey <levin.penza@yandex.ru>
2025-06-17 08:52:05 +0300

    second task finish

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -6,19 +6,31 @@ import com.opencsv.bean.CsvToBeanBuilder;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+import org.w3c.dom.*;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.*;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        //Задание 1 CSV - JSON
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
-        String fileName = "data.csv";
+        String csvFileName = "data.csv";
 
-        List<Employee> list = parseCSV(columnMapping, fileName);
-        String json = listToJson(list);
-        writeString(json, "data.json");
+        List<Employee> listFromCsv = parseCSV(columnMapping, csvFileName);
+        String jsonFromCsv = listToJson(listFromCsv);
+        writeString(jsonFromCsv, "data.json");
+
+        //Задание 2 XML - JSON
+        String xmlFileName = "data.xml";
+
+        List<Employee> listFromXml = parseXML(xmlFileName);
+        String jsonFromXml = listToJson(listFromXml);
+        writeString(jsonFromXml, "data2.json");
     }
 
     public static List<Employee> parseCSV(String[] columnMapping, String fileName) {
@@ -36,8 +48,43 @@ public class Main {
             return csvToBean.parse();
         } catch (IOException e) {
             e.printStackTrace();
-            return null;
+            return new ArrayList<>();
         }
+    }
+
+    public static List<Employee> parseXML(String fileName) {
+        List<Employee> employees = new ArrayList<>();
+
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(new File(fileName));
+            doc.getDocumentElement().normalize();
+
+            NodeList nodeList = doc.getElementsByTagName("employee");
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+
+                    long id = Long.parseLong(element.getElementsByTagName("id").item(0).getTextContent());
+                    String firstName = element.getElementsByTagName("firstName").item(0).getTextContent();
+                    String lastName = element.getElementsByTagName("lastName").item(0).getTextContent();
+                    String country = element.getElementsByTagName("country").item(0).getTextContent();
+                    int age = Integer.parseInt(element.getElementsByTagName("age").item(0).getTextContent());
+
+                    Employee employee = new Employee(id, firstName, lastName, country, age);
+                    employees.add(employee);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return employees;
     }
 
     public static String listToJson(List<Employee> list) {
